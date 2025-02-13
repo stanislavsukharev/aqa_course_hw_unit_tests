ef263a548161170ec7e53dfaf8a9e84a51845519
Stanislav Sukharev <stas.sukharev@icloud.com>
2025-02-14 01:37:12 +0400

    Added HW-4

--- homework-solutions/js/hw-4/task-1.js
+++ homework-solutions/js/hw-4/task-1.js
@@ -3,3 +3,8 @@
  - Создайте переменную salary со значением 1000
   - Создайте переменную grade, которая должна получить значение "middle" если salary больше или равна 1000, и значение "junior" - если меньше
   */
+
+  let salary = 1000;
+  let grade = salary >= 1000 ? "middle" : "junior";
+
+  console.log(grade);
--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -6,3 +6,19 @@
   преобразовываясь в number
 
 */
+
+let minAge = 18;
+  let maxAge = 60;
+  let age = "17";
+
+if (typeof age !== "number" && typeof age !== "string") {
+  console.log("Incorrect data type");
+} else if (String(age) < minAge){
+  console.log("You don't have access cause your age is " + age + " It's less then ");
+} else if (String(age) >= minAge && String(age) < maxAge){
+  console.log("Welcome  !");
+} else if (String(age) > maxAge ){
+  console.log("Keep calm and look Culture channel");
+} else {
+  console.log("Technical work");
+}
