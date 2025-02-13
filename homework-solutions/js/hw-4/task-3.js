faf45e733f0cb903c2ef995f47cd9e33bc02edff
Stanislav Sukharev <stas.sukharev@icloud.com>
2025-02-14 01:59:03 +0400

    Updated HW-4

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -9,15 +9,14 @@
 
 let minAge = 18;
   let maxAge = 60;
-  let age = "17";
+  let age = "17a";
+  age = parseInt(age);
 
-if (typeof age !== "number" && typeof age !== "string") {
-  console.log("Incorrect data type");
-} else if (String(age) < minAge){
+if (age < minAge){
   console.log("You don't have access cause your age is " + age + " It's less then ");
-} else if (String(age) >= minAge && String(age) < maxAge){
+} else if (age >= minAge && String(age) < maxAge){
   console.log("Welcome  !");
-} else if (String(age) > maxAge ){
+} else if (age > maxAge ){
   console.log("Keep calm and look Culture channel");
 } else {
   console.log("Technical work");
