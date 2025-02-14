812c2b9ae86d46b619a74b700ebe4c3663262554
Stanislav Sukharev <stas.sukharev@icloud.com>
2025-02-14 09:28:27 +0400

    Add task-3 remarks

--- homework-solutions/js/hw-4/task-1.js
+++ homework-solutions/js/hw-4/task-1.js
@@ -4,7 +4,7 @@
   - Создайте переменную grade, которая должна получить значение "middle" если salary больше или равна 1000, и значение "junior" - если меньше
   */
 
-  let salary = 1000;
-  let grade = salary >= 1000 ? "middle" : "junior";
+let salary = 1000;
+let grade = salary >= 1000 ? 'middle' : 'junior';
 
-  console.log(grade);
+console.log(grade);
--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -8,16 +8,19 @@
 */
 
 let minAge = 18;
-  let maxAge = 60;
-  let age = "17a";
-  age = parseInt(age);
+let maxAge = 60;
+let age = '123bb';
 
-if (age < minAge){
+if (!isNaN(age) && age !== '') {
+  age = Number(age);
+}
+
+if (age < minAge) {
   console.log("You don't have access cause your age is " + age + " It's less then ");
-} else if (age >= minAge && String(age) < maxAge){
-  console.log("Welcome  !");
-} else if (age > maxAge ){
-  console.log("Keep calm and look Culture channel");
+} else if (age >= minAge && String(age) < maxAge) {
+  console.log('Welcome  !');
+} else if (age > maxAge) {
+  console.log('Keep calm and look Culture channel');
 } else {
-  console.log("Technical work");
+  console.log('Technical work');
 }
