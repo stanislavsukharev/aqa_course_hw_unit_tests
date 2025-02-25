1afb3b8e52557e1e691100d8da38698cfe287487
Stanislav Sukharev <stas.sukharev@icloud.com>
2025-02-25 23:57:00 +0400

    update: hw-6

--- homework-solutions/js/hw-6/task-3.js
+++ homework-solutions/js/hw-6/task-3.js
@@ -6,23 +6,13 @@
 
   Присвойте результат в переменную "unique"
 */
-let unique;
+let unique = [];
 
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 4, 6, 8, 10, 1, 3, 5, 7, 9];
 
-unique = [];
-
 for (let i = 0; i < numbers.length; i++) {
-  let isDuplicate = false;
-
-  for (let j = 0; j < unique.length; j++) {
-    if (numbers[i] === unique[j]) {
-      isDuplicate = true;
-      break;
-    }
-  }
-
-  if (!isDuplicate) {
+  // If the number is not in `unique`, add it
+  if (!unique.includes(numbers[i])) {
     unique.push(numbers[i]);
   }
 }
