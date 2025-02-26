eef365ae032743db2648f538f76a9c972cc25491
Stanislav Sukharev <stas.sukharev@icloud.com>
2025-02-26 22:46:36 +0400

    add: HW-7

--- homework-solutions/js/hw-7/task-1.js
+++ homework-solutions/js/hw-7/task-1.js
@@ -5,9 +5,14 @@
   - Например: mergeArrays([1,2], [3,4], [5,6]) // [1,2,3,4,5,6]
   - Решить с использованием Spread operator
 */
-function mergeArrays() {
-  // Ваш код
+function mergeArrays(...args) {
+  return [].concat(...args);
 }
+
+// Example usage:
+console.log(mergeArrays([1, 2, 3], [4, 5], [6, 7, 8])); 
+// Output: [1, 2, 3, 4, 5, 6, 7, 8]
+
 /*
   2. Devide by _
     - Написать функцию, которая преобразует любое предложение в вот_Такой_Вот_Вид и возвращает его. 
@@ -15,8 +20,24 @@ function mergeArrays() {
     - Пример: I am super engineer => i_Am_Super_Engineer
   */
 function devideBy(sentence) {
-  // Ваш код
+  let words = sentence.trim().split(' ');
+  let result = '';
+
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+
+    if (word === '') continue;
+
+    if (result === '') {
+      result = word.toLowerCase(); 
+    } else {
+      result += '_' + word.charAt(0).toUpperCase() + word.slice(1).toLowerCase(); 
+    }
+  }
+
+  return result;
 }
+
 /*
   3. Фибаначчи
     - Напишите функцию fibonacci(n), возвращающую энное число Фибоначчи
@@ -24,8 +45,19 @@ function devideBy(sentence) {
     первые два числа которой являются 0 и 1, а каждое последующее за ними число является суммой двух предыдущих
     - Например fibonacci(8) //21
   */
-function fibonacci(n) {
-  // Ваш код
-}
+    function fibonacci(n) {
+      if (n <= 1) return n;
+
+      let previous = 0; 
+      let current = 1;  
+    
+      for (let i = 2; i <= n; i++) {
+        let next = previous + current; 
+        previous = current; 
+        current = next;
+      }
+    
+      return current;
+    }
 
 export { mergeArrays, fibonacci, devideBy };
