9fdaa6d670ceb25be6ac6e80833ff757a5396f49
Stanislav Sukharev <stas.sukharev@icloud.com>
2025-03-22 15:25:27 +0400

    HW-11: classes

--- homework-solutions/js/hw-11/task-1.mjs
+++ homework-solutions/js/hw-11/task-1.mjs
@@ -1,9 +1,121 @@
 class Employee {
-  // Ваш код
+  #salary;
+
+  constructor(firstName, lastName, profession, salary) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.profession = profession;
+    this.salary = salary;
+  }
+
+  get firstName() {
+    return this._firstName;
+  }
+
+  set firstName(value) {
+    if (typeof value !== 'string') {
+      throw new Error('First name must be a string');
+    }
+    this._firstName = value;
+  }
+
+  get lastName() {
+    return this._lastName;
+  }
+
+  set lastName(value) {
+    if (typeof value !== 'string') {
+      throw new Error('Last name must be a string');
+    }
+    this._lastName = value;
+  }
+
+  get profession() {
+    return this._profession;
+  }
+
+  set profession(value) {
+    if (typeof value !== 'string') {
+      throw new Error('Profession must be a string');
+    }
+    this._profession = value;
+  }
+
+  get salary() {
+    return this.#salary;
+  }
+
+  set salary(value) {
+    if (typeof value !== 'number' || value <= 0) {
+      throw new Error('Salary must be a positive number');
+    }
+    this.#salary = value;
+  }
+
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
 }
 
 class Company {
-  // Ваш код
+  #employees;
+
+  constructor(title, phone, address) {
+    this.title = title;
+    this.phone = phone;
+    this.address = address;
+    this.#employees = [];
+  }
+
+  get title() {
+    return this._title;
+  }
+
+  set title(value) {
+    if (typeof value !== 'string') {
+      throw new Error('Title must be a string');
+    }
+    this._title = value;
+  }
+
+  get phone() {
+    return this._phone;
+  }
+
+  set phone(value) {
+    if (typeof value !== 'string') {
+      throw new Error('Phone must be a string');
+    }
+    this._phone = value;
+  }
+
+  get address() {
+    return this._address;
+  }
+
+  set address(value) {
+    if (typeof value !== 'string') {
+      throw new Error('Address must be a string');
+    }
+    this._address = value;
+  }
+
+  addEmployee(employee) {
+    if (!(employee instanceof Employee)) {
+      throw new Error('Must be an instance of Employee');
+    }
+
+    this.#employees.push(employee);
+    console.log(`Employee ${employee.firstName} ${employee.lastName} added to ${this.title}`);
+  }
+
+  getEmployees() {
+    return this.#employees;
+  }
+
+  getInfo() {
+    return `Компания: ${this.title}\nАдрес: ${this.address}\nКоличество сотрудников: ${this.#employees.length}`;
+  }
 }
 
 export { Employee, Company };
